9a59b3022683cab10628015ce4afb97222b0d4ab
harrynewt0n <harry-newton@outlook.com>
2024-01-23 19:00:54 +0100

    Created proper file structure

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+// Display time at top of application
+
+$('#currentDay').text(dayjs().format('DD/MM/YYYY'));
+
+console.log(dayjs().format('DD/MM/YYYY'));
+
+// change class based on past, present, future
+
+function updateHour () {
+var currentHour = dayjs().hour()
+$('.time-block').each(function() {
+var rowTime = parseInt(
+    $(this).attr('id').split('-')[1]
+)
+if (rowTime < currentHour) {
+    $(this).addClass('past')
+}
+else if (currentHour === rowTime) {
+    $(this).removeClass('past')
+    $(this).addClass('present')
+}
+else {
+    $(this).removeClass('past')
+    $(this).removeClass('present')
+    $(this).addClass('future')
+}
+})
+} 
+updateHour()
+
+// Allow user to save input to local storage- localstorage.setitem
+
+$('.saveBtn').on('click', function () {
+
+    var scheduleHour = $(this).siblings('.hour').text();
+    var userInput = $(this).siblings('textarea').val();
+
+    console.log(scheduleHour + '- ' + userInput);
+
+    localStorage.setItem(scheduleHour, userInput);
+
+    window.alert('Your task has been created!')
+})
+
+// Ensure that local storage remains on application text field- localstorage.getitem
+
+$('textarea').each(function () {
+    var scheduleHour = $(this).siblings('.hour').text();
+    $(this).val(localStorage.getItem(scheduleHour));
+})
